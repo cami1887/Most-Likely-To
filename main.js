d50481060411eda4b4be64052e2c25ac5d21d25d
Camille Lacaden <cami1887@gmail.com>
2022-11-23 21:29:02 -0800

    updated code based on review
    will require further revision

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let btn = document.querySelector("reset");
+const mostLikelys = ['Commit A Crime', 'Get Scammed', 'Have A Coffee Addiction', 'Get Divorced', 'Acquire A Mental Illness', 'Get Catfished', 'Be On Reality TV', 'Spontaneously Combust', 'Join A Pyramid Scheme', 'Flee the Country'];
 
 function getWords() {
-	const word = ['Commit A Crime', 'Get Scammed', 'Have A Coffee Addiction', 'Get Divorced', 'Acquire A Mental Illness', 'Get Catfished', 'Be On Reality TV', 'Spontaneously Combust', 'Join A Pyramid Scheme', 'Flee the Country'];
-	let words = word[Math.floor(Math.random() * word.length)];
-	let generatedWords = words;
-	document.getElementById("output").innerHTML = generatedWords;
-	console.log(generatedWords);
+	let randomOutput = mostLikelys[Math.floor(Math.random() * mostLikelys.length)];
+	document.getElementById("output").innerHTML = randomOutput;
 }
 
 function openNav() {
@@ -17,25 +14,25 @@ function closeNav() {
 }
 
 function certificateOne() {
-	document.querySelector(".whiteBackground").style.backgroundColor = "white";
-	document.querySelector(".whiteBackground").style.borderStyle = "double";
-	document.querySelector(".whiteBackground").style.borderColor = "rgb(224, 165, 0)";
+	document.querySelector(".cert-background").style.backgroundColor = "white";
+	document.querySelector(".cert-background").style.borderStyle = "double";
+	document.querySelector(".cert-background").style.borderColor = "rgb(224, 165, 0)";
 }
 
 function certificateTwo() {
-	document.querySelector(".whiteBackground").style.backgroundColor = "#9b9b9b60";
-	document.querySelector(".whiteBackground").style.borderStyle = "groove";
-	document.querySelector(".whiteBackground").style.borderColor = "white";
+	document.querySelector(".cert-background").style.backgroundColor = "#9b9b9b60";
+	document.querySelector(".cert-background").style.borderStyle = "groove";
+	document.querySelector(".cert-background").style.borderColor = "white";
 }
 
 function certificateThree() {
-	document.querySelector(".whiteBackground").style.backgroundColor = "#eecd90a1";
-	document.querySelector(".whiteBackground").style.borderStyle = "dashed";
-	document.querySelector(".whiteBackground").style.borderColor = "white";
+	document.querySelector(".cert-background").style.backgroundColor = "#eecd90a1";
+	document.querySelector(".cert-background").style.borderStyle = "dashed";
+	document.querySelector(".cert-background").style.borderColor = "white";
 }
 
 function certificateFour() {
-	document.querySelector(".whiteBackground").style.backgroundColor = "#ffe8e8";
-	document.querySelector(".whiteBackground").style.borderStyle = "dotted";
-	document.querySelector(".whiteBackground").style.borderColor = "white";
+	document.querySelector(".cert-background").style.backgroundColor = "#ffe8e8";
+	document.querySelector(".cert-background").style.borderStyle = "dotted";
+	document.querySelector(".cert-background").style.borderColor = "white";
 }
