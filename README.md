a75611d5900667994416045f1ebf5ce3a3783ee4
Camille Lacaden <cami1887@gmail.com>
2022-12-01 23:29:46 -0800

    Shortened CSS and JS
    Could be made more efficient, Tried my best.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Most Likely To...
 
-High School Senior Award Generator. Begins at a home page where you can click the button that sends you to the generator. Once the generator is loaded, an 'award' is shown in which you can reroll to get a more desirable response. Using the side bar on the generator page, you can choose between certificates types. You can also click the title at the top of the page to be sent back to the home page. This project is designed for desktop users and may be altered in the future to accomodate mobile use as well as to make code more efficient. While not perfect, i built this site to demonstrate basic HTML, CSS, and JS knowledge.
+High School Senior Award Generator. Begins at a home page where you can click the button that sends you to the generator. Once the generator is loaded, an 'award' is shown in which you can reroll to get a more desirable response. Using the side bar on the generator page, you can choose between certificates types. You can also click the title at the top of the page to be sent back to the home page. This project is designed for desktop users and will continually be altered to accommodate mobile use as well as to make code more efficient. While not perfect, I built this site to demonstrate basic HTML, CSS, and JS knowledge.
 
 
--- main.js
+++ main.js
@@ -5,34 +5,35 @@ function getWords() {
 	document.getElementById("output").innerHTML = randomOutput;
 }
 
-function openNav() {
-	document.getElementById("mySidenav").style.width = "350px";
+function openNavigation() { // I attempted to combine open/close into one function, but the buttons would still appear after closing the navigation bar. I assume its because the styling was the only thing being removed. I decided to keep it two functions so I don't add more code anywhere else.
+	document.getElementById("mySidenavigation").style.width = "350px";
 }
 
-function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
+function closeNavigation() {
+	document.getElementById("mySidenavigation").style.width = "0";
 }
 
-function certificateOne() {
-	document.querySelector(".cert-background").style.backgroundColor = "white";
-	document.querySelector(".cert-background").style.borderStyle = "double";
-	document.querySelector(".cert-background").style.borderColor = "rgb(224, 165, 0)";
-}
-
-function certificateTwo() {
-	document.querySelector(".cert-background").style.backgroundColor = "#9b9b9b60";
-	document.querySelector(".cert-background").style.borderStyle = "groove";
-	document.querySelector(".cert-background").style.borderColor = "white";
-}
-
-function certificateThree() {
-	document.querySelector(".cert-background").style.backgroundColor = "#eecd90a1";
-	document.querySelector(".cert-background").style.borderStyle = "dashed";
-	document.querySelector(".cert-background").style.borderColor = "white";
-}
-
-function certificateFour() {
-	document.querySelector(".cert-background").style.backgroundColor = "#ffe8e8";
-	document.querySelector(".cert-background").style.borderStyle = "dotted";
-	document.querySelector(".cert-background").style.borderColor = "white";
+function updateContainer(theme) {
+	const container = document.getElementById('certificate-container');
+	
+	if (theme === 'light-theme') {
+		container.classList.remove('dark-theme');
+		container.classList.remove('gold-theme');
+		container.classList.remove('pink-theme');
+	}
+	if (theme === 'dark-theme') {
+		container.classList.add('dark-theme');
+		container.classList.remove('gold-theme');
+		container.classList.remove('pink-theme');
+	}
+	if (theme === 'gold-theme') {
+		container.classList.add('gold-theme');
+		container.classList.remove('dark-theme');
+		container.classList.remove('pink-theme');
+	}
+	if (theme === 'pink-theme') {
+		container.classList.add('pink-theme');
+		container.classList.remove('dark-theme');
+		container.classList.remove('gold-theme');
+	}
 }
